98802613987fe94c69d4f2f1fa2bf8c42acd6ede
sriram5N2 <139240678+sriram5N2@users.noreply.github.com>
2024-11-07 18:10:55 +0530

    Delete routes directory

--- routes/candidateRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getCandidatesList } = require("../controllers/candidateControllers");
-
-router.get("/list", getCandidatesList);
-
-module.exports = router;
--- routes/votesRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { castVote, voteCount } = require("../controllers/voteControllers");
-
-router.post("/castvote", castVote);
-router.get("/votecount", voteCount);
-
-module.exports = router;
