8979d6925f42c65658c727642de2f1636d54c2a4
sriram5N2 <139240678+sriram5N2@users.noreply.github.com>
2024-11-08 10:39:04 +0530

    Update Context.jsx

--- frontend/src/Context/Context.jsx
+++ frontend/src/Context/Context.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState } from "react";
 export const StatusContext = createContext();
 
 const StatusProvider = ({ children }) => {
-    const backendUrl = "https://giribabi-vote-easy-api.onrender.com";
+    const backendUrl = "https://online-voitng.onrender.com";
     const [progress, setProgress] = useState(0);
 
     return (
